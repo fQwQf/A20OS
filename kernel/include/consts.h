d07fbece83650b9020037d5e1623dab9cbc308f1
fQwQf <supertjz123@foxmail.com>
2026-04-14 19:15:10 +0800

    revert: Revert 3 broken commits (fix: branch rebase fix, feat(arch): support loongarch irq, feat(arch): abstract platform interfaces)

--- kernel/include/consts.h
+++ kernel/include/consts.h
@@ -20,13 +20,25 @@
 #define MAX_CMD_LEN        1024
 #define MAX_HISTORY        64
 
-#if defined(CONFIG_RISCV64)
-#include "riscv64_consts.h"
-#elif defined(CONFIG_LOONGARCH64)
-#include "loongarch64_consts.h"
-#else
-#error "Unsupported architecture: expected CONFIG_RISCV64 or CONFIG_LOONGARCH64"
-#endif
+#define PHYS_MEMORY_BASE   0x80000000UL
+#define PHYS_MEMORY_END    0x88000000UL
+#define KERNEL_ENTRY       0x80200000UL
+
+#define UART0_BASE         0x10000000UL
+#define CLINT_BASE         0x02000000UL
+#define VIRTIO_BASE        0x10001000UL
+#define PLIC_BASE          0x0C000000UL
+#define UART0_IRQ          10
+
+#define PLIC_PRIORITY      (PLIC_BASE + 0x0000UL)
+#define PLIC_PENDING       (PLIC_BASE + 0x1000UL)
+#define PLIC_SENABLE(h)    (PLIC_BASE + 0x2080UL + (uint64_t)(h) * 0x100UL)
+#define PLIC_SPRIORITY(h)  (PLIC_BASE + 0x201000UL + (uint64_t)(h) * 0x2000UL)
+#define PLIC_SCLAIM(h)     (PLIC_BASE + 0x201004UL + (uint64_t)(h) * 0x2000UL)
+
+#define CLINT_MTIME        (CLINT_BASE + 0xBFF8UL)
+#define CLINT_MTIMECMP(h)  (CLINT_BASE + 0x4000UL + ((unsigned long)(h) * 8))
+#define CLINT_TIMER_FREQ   10000000UL
 
 #define KERNEL_HEAP_SIZE   (32 * 1024 * 1024)
 
