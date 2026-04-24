79f41eed453ba39d3a5ec675648fec468dcf5b5c
Guo Yiyan <965845592@qq.com>
2026-04-24 07:45:00 +0000

    contest: establish the test runner framework

---
+++ user/contest_init/common_runner.c
@@ -0,0 +1,41 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "common_runner.h"
+
+int run_script_in_dir(const char *script_name, const char *script_dir, const char *tag)
+{
+    int pid = fork();
+    if (pid < 0)
+        return 127;
+
+    if (pid == 0) {
+        if (chdir(script_dir) != 0) {
+            printf("[%s] chdir(%s) failed errno=%d\n", tag, script_dir, errno);
+            _exit(127);
+        }
+
+        char *envp[] = {
+            "PATH=.:/bin:/test:/test/glibc:/test/musl",
+            "HOME=/",
+            NULL,
+        };
+
+        char *argv[] = {"mksh", (char *)script_name, NULL};
+        execve("/bin/mksh", argv, envp);
+
+        printf("[%s] mksh exec failed: dir=%s script=%s errno=%d\n",
+               tag, script_dir, script_name, errno);
+        _exit(127);
+    }
+
+    int status = 0;
+    if (waitpid(pid, &status, 0) < 0)
+        return 127;
+
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    return 127;
+}
---
+++ user/contest_init/musl_tests.c
@@ -0,0 +1,13 @@
+#include "common_runner.h"
+#include "test_runners.h"
+
+int run_musl_basic_test(const char *script_name, const char *script_dir)
+{
+    return run_script_in_dir(script_name, script_dir, "TEST][musl][basic");
+}
+
+int run_musl_busybox_test(const char *script_name, const char *script_dir)
+{
+    return run_script_in_dir(script_name, script_dir, "TEST][musl][busybox");
+}
+
