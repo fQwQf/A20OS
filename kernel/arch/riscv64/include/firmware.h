3a75313cb81feb34dc6eaabab09ecbe4a4e1e09a
fQwQf <supertjz123@foxmail.com>
2026-06-02 15:42:06 +0800

    feat: implement SMP multi-core infrastructure with HAL abstraction

--- kernel/arch/riscv64/include/firmware.h
+++ kernel/arch/riscv64/include/firmware.h
@@ -10,6 +10,7 @@
 #define SBI_SRST_EID            0x53525354UL
 #define SBI_SRST_SHUTDOWN       0
 #define SBI_SRST_COLD_REBOOT    1
+#define SBI_SEND_IPI_EID        4 /* SBI legacy IPI: 向指定 hart 发送核间中断 */
 
 void firmware_set_timer(uint64_t time);
 void firmware_console_putchar(char c);
@@ -23,5 +24,6 @@ void sbi_console_putchar(char c);
 int  sbi_console_getchar(void);
 void sbi_shutdown(void);
 void sbi_reboot(void);
+void sbi_send_ipi(uint64_t hart_mask);
 
 #endif
