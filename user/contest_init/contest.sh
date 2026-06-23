f13845df3b92e17ec63190af6b4e8d46c1d55f68
Mulei Zhang <muleizh@outlook.com>
2026-06-24 02:20:23 +0800

    contest: guard LTP timing and timeout cases

--- user/contest_init/contest.sh
+++ user/contest_init/contest.sh
@@ -17,6 +17,7 @@ export LTP_TMPDIR=/tmp
 export TMPDIR=/tmp
 export HOME=/root
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}/bin/lib:/lib"
+export PATH="/tmp:$PATH"
 mkdir -p /root
 
 
@@ -26,6 +27,18 @@ chmod 755 /bin/zcat
 print '#!/bin/mksh' > /bin/gunzip
 print 'exec /busybox gunzip "$@"' >> /bin/gunzip
 chmod 755 /bin/gunzip
+print '#!/bin/mksh' > /tmp/systemd-detect-virt
+print 'typeset -i quiet=0 container=0' >> /tmp/systemd-detect-virt
+print 'for arg in "$@"; do' >> /tmp/systemd-detect-virt
+print '    case "$arg" in' >> /tmp/systemd-detect-virt
+print '        --quiet|-q) quiet=1 ;;' >> /tmp/systemd-detect-virt
+print '        --container|-c) container=1 ;;' >> /tmp/systemd-detect-virt
+print '    esac' >> /tmp/systemd-detect-virt
+print 'done' >> /tmp/systemd-detect-virt
+print '(( container )) && exit 1' >> /tmp/systemd-detect-virt
+print '(( quiet )) || print qemu' >> /tmp/systemd-detect-virt
+print 'exit 0' >> /tmp/systemd-detect-virt
+chmod 755 /tmp/systemd-detect-virt
 
 sync
 
@@ -260,8 +273,41 @@ run_ltp_case_with_timeout() {
 
     print "[CONTEST][LTP][TIMEOUT] case=$name after ${timeout}s"
     /busybox killall "$name" 2>/dev/null || killall "$name" 2>/dev/null
+    kill -TERM "$pid" 2>/dev/null
+    sleep 1
+    /busybox killall -9 "$name" 2>/dev/null || killall -9 "$name" 2>/dev/null
+    kill -KILL "$pid" 2>/dev/null
+    wait "$pid" 2>/dev/null
     print "FAIL LTP CASE $name : 124"
     return 1
+}
+
+ltp_case_timeout() {
+    case "$1" in
+        ftest03|ftest04|ftest07|ftest08) print 120 ;;
+        futex_cmp_requeue01) print 180 ;;
+        *) print 60 ;;
+    esac
+}
+
+run_ltp_bounded_case() {
+    typeset name=$1
+    typeset -i timeout=$(ltp_case_timeout "$name")
+    if [[ $name == futex_cmp_requeue01 ]]; then
+        typeset old_mul="${LTP_TIMEOUT_MUL-}"
+        typeset -i had_mul=0
+        [[ -n ${LTP_TIMEOUT_MUL+x} ]] && had_mul=1
+        export LTP_TIMEOUT_MUL=4
+        run_ltp_case_with_timeout "$name" "$timeout"
+        typeset rc=$?
+        if (( had_mul )); then
+            export LTP_TIMEOUT_MUL="$old_mul"
+        else
+            unset LTP_TIMEOUT_MUL
+        fi
+        return $rc
+    fi
+    run_ltp_case_with_timeout "$name" "$timeout"
 }
 
 run_ltp_bounded_subset() {
@@ -274,7 +320,7 @@ run_ltp_bounded_subset() {
         return 0
     fi
 
-    print "[CONTEST][RUN] runtime=$runtime group=ltp mode=bounded_subset case_timeout=60s"
+    print "[CONTEST][RUN] runtime=$runtime group=ltp mode=bounded_subset case_timeout=60s ftest_timeout=120s futex_timeout=180s futex_ltp_timeout_mul=4"
     print "#### OS COMP TEST GROUP START ltp-$runtime ####"
 
     if [[ ! -d $dir ]]; then
@@ -309,10 +355,10 @@ run_ltp_bounded_subset() {
         capset01 capset02 capset03 capset04 \
         cgroup_core03
     do
-        run_ltp_case_with_timeout "$name" 60 || (( failed_cases++ ))
+        run_ltp_bounded_case "$name" || (( failed_cases++ ))
     done
     print "[CONTEST][LTP][SKIP] cgroup_fj_proc blacklisted_helper"
-    run_ltp_case_with_timeout chdir04 60 || (( failed_cases++ ))
+    run_ltp_bounded_case chdir04 || (( failed_cases++ ))
     for name in \
         chmod01 chmod03 chmod05 chmod06 chmod07 \
         chown01 chown02 chown03 chown04 chown05 \
@@ -546,7 +592,7 @@ run_ltp_bounded_subset() {
         clone01 diotest1 diotest4 memcontrol01 mmap001 \
         dirty doio epoll-ltp fs_racer.sh
     do
-        run_ltp_case_with_timeout "$name" 60 || (( failed_cases++ ))
+        run_ltp_bounded_case "$name" || (( failed_cases++ ))
     done
 
     cd /
