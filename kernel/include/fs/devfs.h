9574e28c86a638b0d2422192bea589173fe574a3
fQwQf <supertjz123@foxmail.com>
2026-07-27 18:24:03 +0800

    fbdev: support mmap

--- kernel/include/fs/devfs.h
+++ kernel/include/fs/devfs.h
@@ -8,6 +8,7 @@ vfile_t *devfs_create_stdio(int fd);
 int devfs_is_char_vfile(vfile_t *vf);
 int devfs_is_tty_vfile(vfile_t *vf);
 int devfs_is_zero_vfile(vfile_t *vf);
+int devfs_is_fb_vfile(vfile_t *vf);
 
 extern vfile_ops_t g_devfs_fb_ops;
 extern vfile_ops_t g_devfs_input_ops;
