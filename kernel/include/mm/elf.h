150c48f8386b3989698ff35c3b30ae1836240e92
fQwQf <supertjz123@foxmail.com>
2026-07-31 19:35:11 +0800

    native: preserve start info across dynamic exec

--- kernel/include/mm/elf.h
+++ kernel/include/mm/elf.h
@@ -177,6 +177,13 @@ vaddr_t elf_setup_stack_a20(vaddr_t stack_top, int argc, char *const argv[],
                              uint32_t stdin_h, uint32_t stdout_h,
                              uint32_t stderr_h, uint32_t self_task_h,
                              uint32_t root_h, uint32_t cwd_h);
+vaddr_t elf_setup_stack_a20_dynamic(vaddr_t stack_top, int argc,
+                                    char *const argv[], char *const envp[],
+                                    const elf_load_info_t *info,
+                                    uint32_t stdin_h, uint32_t stdout_h,
+                                    uint32_t stderr_h, uint32_t self_task_h,
+                                    uint32_t root_h, uint32_t cwd_h,
+                                    vaddr_t *start_info_out);
 #endif
 
 #endif /* _ELF_H */
