358b8adc32e99293141d3ed19b88e5753857e40a
Mulei Zhang <muleizh@outlook.com>
2026-08-05 21:09:22 +0800

    build: restore cross-architecture preflight gates

--- kernel/arch/ppc64le/include/cpu.h
+++ kernel/arch/ppc64le/include/cpu.h
@@ -110,6 +110,9 @@ static inline void arch_tlb_flush_page(uint64_t addr) {
      * freshly installed PTE; fall back to the full IS-form invalidation. */
     arch_tlb_flush();
 }
+static inline void arch_tlb_flush_page_local(uint64_t addr) {
+    arch_tlb_flush_page(addr);
+}
 
 static inline void arch_set_task_pointer(void *task) {
     __asm__ __volatile__("mr 13,%0" :: "r"(task));
--- kernel/arch/riscv64/include/arch.h
+++ kernel/arch/riscv64/include/arch.h
@@ -1,6 +1,8 @@
 #ifndef _ARCH_RISCV64_H
 #define _ARCH_RISCV64_H
 
+#define ARCH_HAS_VDSO 1
+
 #include "platform.h"
 #include "console.h"
 #include "cpu.h"
