3a8d1070fedf7ded9d2d6550941f2f313a6097fe
Mulei Zhang <muleizh@outlook.com>
2026-08-10 13:22:41 +0800

    build: make rootfs overlay generation clone-safe

--- tools/gen_rootfs_overlay.py
+++ tools/gen_rootfs_overlay.py
@@ -41,6 +41,9 @@ typedef struct {
 
 extern const rootfs_overlay_entry_t g_rootfs_overlay[];
 extern const size_t g_rootfs_overlay_count;
+extern rootfs_overlay_entry_t g_rootfs_driver_overlay[];
+extern const size_t g_rootfs_driver_overlay_count;
+void rootfs_driver_overlay_init(void);
 
 #endif
 """,
